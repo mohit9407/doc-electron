37f6352121c218c14482bdbb44f50e6d2fa203ab
Manthan0997 <manthan.gadhiya.thinketic@gmail.com>
2024-08-06 17:49:18 +0530

    add pagination and created patient data api call

---
+++ src/app/api/patients/manage/route.js
@@ -0,0 +1,20 @@
+import { NextResponse, NextRequest } from "next/server";
+import { readFileSync } from "fs";
+/**
+ * @param {NextRequest} req
+ */
+export async function GET() {
+  try {
+    const patientRecords = JSON.parse(
+      readFileSync(`/home/admin/Desktop/app.json`, "utf8") || null
+    );
+    const patientInfo = patientRecords?.patientInfo;
+
+    if (patientInfo?.length) {
+      return NextResponse.json({ data: patientInfo }, { status: 200 });
+    }
+    return NextResponse.json({ message: "no record found" }, { status: 404 });
+  } catch (error) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+}
