eb81f9890795c00e6b9f2b5a538156cc734c0232
mohit <taylormohit9997@gmail.com>
2024-08-07 21:06:07 +0530

    pdf downlaod done

--- src/app/api/patients/manage/route.js
+++ src/app/api/patients/manage/route.js
@@ -1,12 +1,14 @@
 import { NextResponse, NextRequest } from "next/server";
 import { readFileSync } from "fs";
+import puppeteer from 'puppeteer';
+import handlers from 'handlebars';
 /**
  * @param {NextRequest} req
  */
 export async function GET() {
   try {
     const patientRecords = JSON.parse(
-      readFileSync(process.env.jsonFilePath, "utf8") || null
+      readFileSync(`C:/Users/admin pc/Documents/app.json`, "utf8") || null
     );
     const patientInfo = patientRecords?.patientInfo;
 
@@ -16,5 +18,35 @@ export async function GET() {
     return NextResponse.json({ message: "no record found" }, { status: 404 });
   } catch (error) {
     return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+}
+
+/**
+ * @param {NextRequest} req
+ */
+export async function POST(req) {
+  const body = await req.json();
+  const { name } = body;
+  const customerName = name || 'John Doe';
+
+  try {
+    // read our invoice-template.html file using node fs module
+    const file = readFileSync('./src/app/api/patients/manage/invoice-template.html', 'utf8');
+    // compile the file with handlebars and inject the customerName variable
+    const template = handlers.compile(`${file}`);
+    const html = template({ customerName });
+    // simulate a chrome browser with puppeteer and navigate to a new page
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    // set our compiled html template as the pages content
+    // then waitUntil the network is idle to make sure the content has been loaded
+    await page.setContent(html, { waitUntil: 'networkidle0' });
+    // convert the page to pdf with the .pdf() method
+    const pdf = await page.pdf({ format: 'A4' });
+    await browser.close();
+    return new Response(pdf, { headers: { 'content-type': 'image/png' } });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({ message: err }, { status: 500 });
   }
 }
--- src/components/patient/payment-table-action.js
+++ src/components/patient/payment-table-action.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import axios from "axios";
 import {
   Dialog,
   DialogHeader,
@@ -10,12 +11,40 @@ import {
 import AddPatientThirdForm from "../staff/add-patient-third-form";
 
 const PaymentTableActions = ({ paymentHistory, updatePaymentHistory }) => {
+  const generateInvoice = (e) => {
+    e.preventDefault();
+    // send a post request with the name to our API endpoint
+    const fetchData = async () => {
+      const { data } = await axios({
+        method: "post",
+        url: "/api/patients/manage",
+        data: { name: "Test data" },
+        responseType: "blob",
+      });
+      // convert the response into an array Buffer
+      return data.arrayBuffer();
+    };
+
+    // convert the buffer into an object URL
+    const saveAsPDF = async () => {
+      const buffer = await fetchData();
+      const blob = new Blob([buffer]);
+      const link = document.createElement("a");
+      link.href = URL.createObjectURL(blob);
+      link.download = "invoice.pdf";
+      link.click();
+    };
+
+    saveAsPDF();
+  };
+
   return (
     <div className="flex flex-row justify-between max-w-sm w-full">
       <Dialog>
         <DialogTrigger asChild>
           <Button>Edit</Button>
         </DialogTrigger>
+        <Button onClick={generateInvoice}>Resept Download</Button>
         <DialogContent className="overflow-y-scroll max-h-screen">
           <DialogHeader>Edit Payment History</DialogHeader>
           <AddPatientThirdForm
