3da40bec43fa96e9837f89670aa202774ff445d1
mohit <taylormohit9997@gmail.com>
2024-08-20 15:33:49 +0530

    added all payments route in new backend

--- src/pages/patients/edit/layout.js
+++ src/pages/patients/edit/layout.js
@@ -1,6 +1,6 @@
 "use client";
-import ClientOnly from "../../../../components/client-only";
-import AppBar from "../../../../components/navbar/app-bar";
+import ClientOnly from "../../../components/client-only";
+import AppBar from "../../../components/navbar/app-bar";
 
 const Layout = ({ children }) => {
   return (
