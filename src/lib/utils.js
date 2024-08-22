f8b82f6fe7b8e3a9db673bf6bf0a7c91027c9693
Manthan0997 <manthan.gadhiya.thinketic@gmail.com>
2024-08-22 18:03:05 +0530

    create common function for date format

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,9 +1,13 @@
 import { clsx } from "clsx";
+import moment from "moment";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export const dateFormat = (date) =>
+  moment(date).format("DD-MM-YYYY HH:mm:ss a");
 
 Array.prototype.extend = function (iterable) {
   if (typeof iterable[Symbol.iterator] === "function") {
